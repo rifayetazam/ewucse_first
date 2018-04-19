60b9b9c972b68eeac9c58dac96b87dfdf77d5418
rifayetazam <rifayettalukder@gmail.com>
2018-04-19 13:03:19 +0600

    implemented add function

--- math-operation.c
+++ math-operation.c
@@ -1,10 +1,14 @@
-#include <bits/stdc++.h>
+#include <stdio.h>
+
+int add(int a, int b){
+    return a+b;
+}
 
 int main()
 {
     int sum =add(3,4);
 
-    printf("sum is %d",sum);
+    printf("sum is %d\n",sum);
 
     return 0;
 }
